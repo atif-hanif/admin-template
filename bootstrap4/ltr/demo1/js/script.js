75e6be4ea2608b7a40f92b0ab9b98ac2f768b118
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2023-07-19 12:24:39 +0500

    19-07-2023

--- bootstrap4/ltr/demo1/js/script.js
+++ bootstrap4/ltr/demo1/js/script.js
@@ -3,14 +3,14 @@ $(function() {
 	$('.sidebar .sidebar-menu li a').on('click', function() {
 		const $this = $(this);
 		if($this.parent().hasClass('open')) {
-			$this.parent().children('.dropdown-menu').slideUp(200, () => {
+			$this.parent().children('.dropdown-menu').slideUp(500, () => {
 				$this.parent().removeClass('open');
 			});
 		} else {
 			$this.parent().parent().children('li.open').children('.dropdown-menu').slideUp(200);
 			$this.parent().parent().children('li.open').children('a').removeClass('open');
 			$this.parent().parent().children('li.open').removeClass('open');
-			$this.parent().children('.dropdown-menu').slideDown(200, () => {
+			$this.parent().children('.dropdown-menu').slideDown(500, () => {
 				$this.parent().addClass('open');
 			});
 		}
