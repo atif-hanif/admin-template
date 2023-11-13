e8dcce9eddcf845070e4c9877c785a05aa337749
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2023-11-13 16:34:44 +0500

    13-11-2023

--- bootstrap4/ltr/demo1/js/script.js
+++ bootstrap4/ltr/demo1/js/script.js
@@ -66,3 +66,21 @@ var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
 jQuery(document).ready(function(e) {
 	jQuery('.sidemenu').find('.current-menu-item').closest('.current-menu-parent').children('.dropdown-toggle').trigger('click');
 });
+
+$(document).ready(function(){
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			$('#back-to-top').fadeIn();
+		} else {
+			$('#back-to-top').fadeOut();
+		}
+	});
+	
+	// scroll body to 0px on click
+	$('#back-to-top').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		return false;
+	});
+});
